303044d9cc370f36cc1232309754ea45274b5c09
vbiar <vitorbiar@gmail.com>
2021-11-11 19:06:34 -0300

    Fechando a calculadora em JS

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,21 @@
-const qS = (element) => { document.querySelector(element)};
-const qSA = (element) => {document.querySelectorAll(element)};
 
-let number1 = qS('number1');
-let number2 = qS('number2');
-let btnCalculate = qS('calculate');
-let res = qS('result');
+// Exemplo de um código que ficaria mais simples com TypeScript
+
+let number1 = document.getElementById('number1');
+let number2 = document.getElementById('number2');
+let btnCalculate = document.getElementById('calculate');
+let res = document.getElementById('result');
 
 
 function calculate (n1, n2) {
-    return n1 + n2;
+    if (typeof n1 == 'number'&& typeof n2 == 'number') {
+
+        return n1 + n2;
+    } else {
+        /* n1 = parseInt(n1);
+        n2 = parseInt(n2); */
+        return +n1 + +n2;
+    }
 };
 
 btnCalculate.addEventListener('click', function() {
